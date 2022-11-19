44fd89cb0f91bfaf8d379483aa855f6643882de9
duy <duynguyen99.work@gmail.com>
2022-11-19 15:29:22 +0700

    add declare modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
         "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+        'node_modules/flowbite/**/*.js',
     ],
     theme: {
         extend: {
@@ -16,5 +17,4 @@ module.exports = {
             },
         },
     },
-    plugins: [require("flowbite/plugin")],
 };
