37d0b91a5c1ddb7412042720923623d6a3f459c5
duy <duynguyen99.work@gmail.com>
2022-11-19 15:49:57 +0700

    remove unsued code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
     Router.events.on("routeChangeError", (url) => {
       setIsLoading(false);
     });
-  }, [Router]);
+  }, []);
 
   return (
     <SessionProvider session={session}>
